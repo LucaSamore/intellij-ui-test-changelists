51ef4690e108299c11909ec1f2a4604fd273bd66
LucaSamore <luca.samore.01@gmail.com>
2026-04-30 23:47:11 +0200

    refactor: test code

--- src/test/kotlin/ChangelistSettingsTest.kt
+++ src/test/kotlin/ChangelistSettingsTest.kt
@@ -1,6 +1,8 @@
+import com.intellij.driver.sdk.ui.components.common.IdeaFrameUI
 import com.intellij.driver.sdk.ui.components.common.ideFrame
 import com.intellij.driver.sdk.ui.components.elements.button
 import com.intellij.driver.sdk.ui.components.elements.checkBoxWithName
+import com.intellij.driver.sdk.ui.components.settings.SettingsDialogUiComponent
 import com.intellij.driver.sdk.ui.components.settings.clickOkBtnAndCloseDialog
 import com.intellij.driver.sdk.ui.components.settings.settingsDialog
 import com.intellij.ide.starter.community.model.BuildType
@@ -19,78 +21,85 @@ import org.junit.jupiter.api.Test
 
 class ChangelistSettingsTest {
 
+    companion object {
+        private const val SETTINGS_GROUP = "Version Control"
+        private const val SETTINGS_SECTION = "Changelists"
+        private const val CHECKBOX_NAME = "Create changelists automatically"
+    }
+
     @Test
-    fun `enable create changelists automatically`() {
-        configureTestContext().runIdeWithDriver().useDriverAndCloseIde {
-            ideFrame {
-                waitForIndicators(5.minutes)
-                openSettingsDialog()
-                settingsDialog {
-                    openTreeSettingsSection("Version Control", "Changelists")
-                    content {
-                        val checkbox = checkBoxWithName("Create changelists automatically")
-                        if (!checkbox.isSelected()) {
-                            checkbox.click()
-                        }
-                        assertTrue(checkbox.isSelected()) { "Checkbox should be selected" }
-                    }
-                    clickOkBtnAndCloseDialog()
+    fun `enable create changelists automatically`() = runTest {
+        withChangelistSettings {
+            content {
+                val checkbox = checkBoxWithName(CHECKBOX_NAME)
+                if (!checkbox.isSelected()) {
+                    checkbox.click()
+                }
+                assertTrue(checkbox.isSelected()) { "Checkbox should be selected" }
+            }
+            clickOkBtnAndCloseDialog()
+        }
+    }
+
+    @Test
+    fun `setting is persisted after clicking OK and reopening settings`() = runTest {
+        withChangelistSettings {
+            content {
+                val checkbox = checkBoxWithName(CHECKBOX_NAME)
+                if (!checkbox.isSelected()) {
+                    checkbox.click()
+                }
+            }
+            clickOkBtnAndCloseDialog()
+        }
+
+        withChangelistSettings {
+            content {
+                val checkbox = checkBoxWithName(CHECKBOX_NAME)
+                assertTrue(checkbox.isSelected()) {
+                    "Checkbox should still be selected after closing and reopening settings"
                 }
             }
         }
     }
 
     @Test
-    fun `setting is persisted after clicking OK and reopening settings`() {
-        configureTestContext().runIdeWithDriver().useDriverAndCloseIde {
-            ideFrame {
-                waitForIndicators(5.minutes)
-                openSettingsDialog()
-                settingsDialog {
-                    openTreeSettingsSection("Version Control", "Changelists")
-                    content {
-                        val checkbox = checkBoxWithName("Create changelists automatically")
-                        if (!checkbox.isSelected()) {
-                            checkbox.click()
-                        }
-                    }
-                    clickOkBtnAndCloseDialog()
-                }
-                openSettingsDialog()
-                settingsDialog {
-                    val checkbox = checkBoxWithName("Create changelists automatically")
-                    assertTrue(checkbox.isSelected()) {
-                        "Checkbox should still be selected after closing and reopening settings"
-                    }
-                }
-            }
-        }
-    }
-
-    @Test
-    fun `setting is not persisted after reopening settings without clicking OK`() {
+    fun `setting is not persisted after reopening settings without clicking OK`() = runTest {
         var initialCheckboxState = false
+
+        withChangelistSettings {
+            content {
+                val checkbox = checkBoxWithName(CHECKBOX_NAME)
+                initialCheckboxState = checkbox.isSelected()
+                checkbox.click()
+            }
+            button("Cancel").click()
+        }
+
+        withChangelistSettings {
+            content {
+                val checkbox = checkBoxWithName(CHECKBOX_NAME)
+                assertEquals(initialCheckboxState, checkbox.isSelected()) {
+                    "Checkbox state should not have changed after clicking Cancel"
+                }
+            }
+        }
+    }
+
+    private fun runTest(testBody: IdeaFrameUI.() -> Unit) {
         configureTestContext().runIdeWithDriver().useDriverAndCloseIde {
             ideFrame {
                 waitForIndicators(5.minutes)
-                openSettingsDialog()
-                settingsDialog {
-                    openTreeSettingsSection("Version Control", "Changelists")
-                    content {
-                        val checkbox = checkBoxWithName("Create changelists automatically")
-                        initialCheckboxState = checkbox.isSelected()
-                        checkbox.click()
-                    }
-                    button("Cancel").click()
-                }
-                openSettingsDialog()
-                settingsDialog {
-                    val checkbox = checkBoxWithName("Create changelists automatically")
-                    assertEquals(initialCheckboxState, checkbox.isSelected()) {
-                        "Checkbox state should not have changed after clicking Cancel"
-                    }
-                }
+                testBody()
             }
+        }
+    }
+
+    private fun IdeaFrameUI.withChangelistSettings(action: SettingsDialogUiComponent.() -> Unit) {
+        openSettingsDialog()
+        settingsDialog {
+            openTreeSettingsSection(SETTINGS_GROUP, SETTINGS_SECTION)
+            action()
         }
     }
 
